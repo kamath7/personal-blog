1cfc9477b8c8caf9e42bb20615c3cbe8ac6a758c
kamath7 <adithyakamath96@gmail.com>
2024-04-22 02:47:04 +0530

    generating titles

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,7 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 router.get("", (req, res) => {
-  res.render("index");
+
+  const locals = {
+    title: "Adithya blog",
+    description: "Random ramblings about somethings I find. Might update regularly"
+  }
+  res.render("index", {locals});
+});
+
+router.get("/about", (req, res) => {
+  res.render("about");
 });
 
 module.exports = router;
